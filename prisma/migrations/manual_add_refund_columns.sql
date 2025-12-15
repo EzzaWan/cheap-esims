c54df1829769bfdfd003c59452f9be413bea5966
Ezza Wan <ezzawan9@gmail.com>
2025-12-15 16:30:20 +0100

    Remove temporary SQL migration file

--- prisma/migrations/manual_add_refund_columns.sql
+++
@@ -1,6 +0,0 @@
--- Add refund-related columns to Order if they don't exist
-ALTER TABLE "Order" ADD COLUMN IF NOT EXISTS "refundAmountCents" INTEGER;
-ALTER TABLE "Order" ADD COLUMN IF NOT EXISTS "refundMethod" TEXT;
-ALTER TABLE "Order" ADD COLUMN IF NOT EXISTS "refundedAt" TIMESTAMPTZ;
-
-
